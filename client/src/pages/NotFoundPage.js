35a1e7702171cd7feb7aa120690fa8ab19bb3afe
Jonatandb <jonatandb@gmail.com>
2022-03-21 18:21:11 -0300

    Styling of Not found page

--- client/src/pages/NotFoundPage.js
+++ client/src/pages/NotFoundPage.js
@@ -1,5 +1,19 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { Col, Container, Row } from 'react-bootstrap'
+import routes from '../helpers/routes'
 
 export default function NotFoundPage() {
-  return <h1>NotFoundPage</h1>
+  return (
+    <Container>
+      <Row className='mt-5'>
+        <Col md={{ span: 6, offset: 3 }} className='text-center'>
+          <img style={{ width: '100%' }} src='/img/404-not-found.svg' alt='error-404' />
+          <h2>¿Te has perdido?</h2>
+          <p>
+            Vuelve al <Link to={routes.home}>inicio</Link>
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
